60c3ef79807b03455249a643659ffb65a47c1517
Sioni Summers <14807534+thesps@users.noreply.github.com>
2026-03-19 21:55:23 +0100

    Try reporting on all ML instances

--- firmware/hdl/payload/gtl/ml_calculation_instances.vhd
+++ firmware/hdl/payload/gtl/ml_calculation_instances.vhd
@@ -42,6 +42,13 @@ architecture rtl of ml_calculation_instances is
 
 begin
 
+    process(lhc_clk)
+    begin
+        if rising_edge(lhc_clk) then
+            report natural'image(obj) & " score : " & integer'image(to_integer(unsigned(ml_score)));
+        end if;
+    end process;
+
     ett_i(MAX_ESUMS_BITS-1 downto 0) <= ett;
     htt_i(MAX_ESUMS_BITS-1 downto 0) <= htt;
     etm_i(MAX_ESUMS_BITS-1 downto 0) <= etm;
